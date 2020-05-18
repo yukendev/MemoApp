fe56a574961c9559dbe61ce414e91a895e035115
Yuken Tezuka <tezuka.y@icloud.com>
2020-05-18 19:13:07 +0900

    Create MemoList

---
+++ src/elements/BodyText.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+class BodyText extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text style={styles.text}>{this.props.children}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: "#DDD",
+    backgroundColor: "#fff",
+  },
+});
+
+export default BodyText;
